f63a96ba53e4989d319661bc10068706ae968a89
Gabriel R. Abreu <grsabreu@gmail.com>
2017-09-24 22:19:14 -0300

    Add components and story

--- stories/index.js
+++ stories/index.js
@@ -1,13 +1,10 @@
-import React from 'react';
+import React from 'react'
 
-import { storiesOf } from '@storybook/react';
-import { action } from '@storybook/addon-actions';
-import { linkTo } from '@storybook/addon-links';
+import { storiesOf } from '@storybook/react'
+import { action } from '@storybook/addon-actions'
+import { linkTo } from '@storybook/addon-links'
 
-import { Button, Welcome } from '@storybook/react/demo';
+import GoogleNews from '../lib/index'
+import { Button, Welcome } from '@storybook/react/demo'
 
-storiesOf('Welcome', module).add('to Storybook', () => <Welcome showApp={linkTo('Button')} />);
-
-storiesOf('Button', module)
-  .add('with text', () => <Button onClick={action('clicked')}>Hello Button</Button>)
-  .add('with some emoji', () => <Button onClick={action('clicked')}>😀 😎 👍 💯</Button>);
+storiesOf('Google News', module).add('Default', () => <GoogleNews queryString="Fullmetal Alchemist" />)

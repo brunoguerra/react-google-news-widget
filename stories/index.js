c3e56394d5986e972e67972389debc6f2462b7d2
Gabriel R. Abreu <grsabreu@gmail.com>
2017-10-02 21:57:11 -0300

    Add knobs

--- stories/index.js
+++ stories/index.js
@@ -3,8 +3,11 @@ import React from 'react'
 import { storiesOf } from '@storybook/react'
 import { action } from '@storybook/addon-actions'
 import { linkTo } from '@storybook/addon-links'
+import { withKnobs, text } from '@storybook/addon-knobs'
 
 import GoogleNews from '../lib/index'
 import { Button, Welcome } from '@storybook/react/demo'
 
-storiesOf('Google News', module).add('Default', () => <GoogleNews queryString="Fullmetal Alchemist" />)
+storiesOf('Google News', module)
+  .addDecorator(withKnobs)
+  .add('Default', () => <GoogleNews queryString={text('Query string', 'Fullmetal Alchemist')} />)
